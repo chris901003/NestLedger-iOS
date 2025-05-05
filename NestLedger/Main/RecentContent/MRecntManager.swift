328eb722b8e24342f14bdcee2c094c64c295404b
chris901003 <a0987999103@gmail.com>
2025-05-05 21:23:46 +0800

    <NestLedger> #30 feat: main recent view set main ledger

--- NestLedger/Main/RecentContent/MRecntManager.swift
+++ NestLedger/Main/RecentContent/MRecntManager.swift
@@ -22,6 +22,7 @@ class MRecntManager {
         NotificationCenter.default.addObserver(self, selector: #selector(receiveNewTransaction), name: .newRecentTransaction, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(receiveUpdateTransaction), name: .updateTransaction, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(receiveDeleteTransaction), name: .deleteTransaction, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(receiveSetMainLedger), name: .setMainLedger, object: nil)
         loadRecentTransaction()
     }
 
@@ -42,6 +43,7 @@ class MRecntManager {
         Task {
             do {
                 recentTransactions = try await apiManager.getTransactionByLedger(config: searchConfig)
+                await MainActor.run { vc?.tableView.reloadData() }
             } catch {
                 XOBottomBarInformationManager.showBottomInformation(type: .failed, information: "無法取得近期帳目")
             }
@@ -50,7 +52,8 @@ class MRecntManager {
 
     @objc private func receiveNewTransaction(_ notification: Notification) {
         guard let userInfo = notification.userInfo,
-              let transaction = userInfo["transaction"] as? TransactionData else { return }
+              let transaction = userInfo["transaction"] as? TransactionData,
+              transaction.ledgerId == ledgerId else { return }
         recentTransactions.insert(transaction, at: 0)
         DispatchQueue.main.async { [weak self] in
             self?.vc?.tableView.reloadData()
@@ -73,5 +76,12 @@ class MRecntManager {
         DispatchQueue.main.async { [weak self] in
             self?.vc?.tableView.reloadData()
         }
+    }
+
+    @objc private func receiveSetMainLedger(_ notification: Notification) {
+        guard let mainLedgerId = NLNotification.decodeSetMainLedger(notification),
+              mainLedgerId != ledgerId else { return }
+        ledgerId = mainLedgerId
+        loadRecentTransaction()
     }
 }
