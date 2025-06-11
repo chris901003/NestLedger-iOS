058a949bd543d436287811083276cb1766f41594
chris901003 <a0987999103@gmail.com>
2025-06-11 21:04:11 +0800

    <NestLedger> #82 feat: update ledger data to version 2
    custom decode ledger data

--- NestLedger/Datas/Ledger/LedgerData.swift
+++ NestLedger/Datas/Ledger/LedgerData.swift
@@ -15,13 +15,40 @@ struct LedgerData: Codable {
     var totalIncome: Int
     var totalExpense: Int
     var version: Int = LEDGER_DATA_VERSION
+    // Version 2
+    var userNames: [String: String]
 
     static func initMock() -> LedgerData {
-        .init(_id: "", title: "", userIds: [], totalIncome: 0, totalExpense: 0)
+        .init(_id: "", title: "", userIds: [], totalIncome: 0, totalExpense: 0, userNames: [:])
     }
 
     var titleShow: String {
         get { title.starts(with: "[Main]") ? "我的帳本" : title }
+    }
+
+    init(_id: String, title: String, userIds: [String], totalIncome: Int, totalExpense: Int, userNames: [String: String]) {
+        self._id = _id
+        self.title = title
+        self.userIds = userIds
+        self.totalIncome = totalIncome
+        self.totalExpense = totalExpense
+        self.userNames = userNames
+    }
+
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        _id = try container.decode(String.self, forKey: ._id)
+        title = try container.decode(String.self, forKey: .title)
+        userIds = try container.decode([String].self, forKey: .userIds)
+        totalIncome = try container.decode(Int.self, forKey: .totalIncome)
+        totalExpense = try container.decode(Int.self, forKey: .totalExpense)
+        version = try container.decode(Int.self, forKey: .version)
+
+        if version < 2 {
+            userNames = [:]
+        } else {
+            userNames = try container.decode([String: String].self, forKey: .userNames)
+        }
     }
 }
 
