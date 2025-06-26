e61f34a2cad5baf7dcb2f24e6602df3bc5053086
chris901003 <a0987999103@gmail.com>
2025-06-26 20:23:23 +0800

    <NestLedger> #89 feat: setting view controller

--- NestLedger/LedgerSplitDetail/LedgerSplitDetailViewController.swift
+++ NestLedger/LedgerSplitDetail/LedgerSplitDetailViewController.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 class LedgerSplitDetailViewController: UIViewController {
+    let settingButton = UIImageView()
+
     let manager: LedgerSplitDetailManager
 
     init(ledgerSplitData: LedgerSplitData) {
@@ -30,9 +32,26 @@ class LedgerSplitDetailViewController: UIViewController {
     private func setup() {
         view.backgroundColor = .white
         navigationItem.title = manager.ledgerSplitData.title
+
+        settingButton.image = UIImage(systemName: "gear")
+        settingButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapSettingAction)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: settingButton)
     }
 
     private func layout() {
         
+    }
+
+    @objc private func tapSettingAction() {
+        let settingViewController = LSDSettingViewController()
+        let navigationVC = UINavigationController(rootViewController: settingViewController)
+        let _50DetentId = UISheetPresentationController.Detent.Identifier("50")
+        let _50Detent = UISheetPresentationController.Detent.custom(identifier: _50DetentId) { context in
+            UIScreen.main.bounds.height * 0.5
+        }
+        if let sheet = navigationVC.sheetPresentationController {
+            sheet.detents = [_50Detent]
+        }
+        present(navigationVC, animated: true)
     }
 }
---
+++ NestLedger/LedgerSplitDetail/Setting/LSDSettingViewController.swift
@@ -0,0 +1,26 @@
+// Created for NestLedger in 2025
+// Using Swift 6.0
+//
+//
+// Created by HongYan on 2025/6/26.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import Foundation
+import UIKit
+
+class LSDSettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        layout()
+    }
+
+    private func setup() {
+        view.backgroundColor = .white
+    }
+
+    private func layout() {
+        
+    }
+}
