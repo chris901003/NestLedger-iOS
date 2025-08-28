37c531ed23bfc483fa3c6fce0fbdf5f858be0cd6
chris901003 <a0987999103@gmail.com>
2025-08-28 21:12:02 +0800

    <NestLedger> #110 feat: feedback scrollable and send button

---
+++ NestLedger/Account/Feedback/FeedbackManager.swift
@@ -0,0 +1,15 @@
+// Created for NestLedger in 2025
+// Using Swift 6.0
+//
+//
+// Created by HongYan on 2025/8/28.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import Foundation
+
+class FeedbackManager {
+    func sendFeedback() async {
+        print("✅ Send feedback")
+    }
+}
--- NestLedger/Account/Feedback/FeedbackViewController.swift
+++ NestLedger/Account/Feedback/FeedbackViewController.swift
@@ -12,24 +12,51 @@ import xxooooxxCommonUI
 
 class FeedbackViewController: UIViewController {
     let topBarView = UIView()
+    let sendButton = XOPaddedImageView(
+        padding: .init(top: 6, left: 6, bottom: 6, right: 6),
+        image: UIImage(systemName: "paperplane")?.withTintColor(.systemBlue, renderingMode: .alwaysOriginal)
+    )
+    let scrollView = UIScrollView()
+    let mainContentView = UIView()
     let titleLable = UILabel()
     let titleInputView = XOTextField(.init(top: 8, left: 16, bottom: 8, right: 16))
     let emailAddressLabel = UILabel()
     let emailAddressInputView = XOTextField(.init(top: 8, left: 16, bottom: 8, right: 16))
     let contentLabel = UILabel()
     let contentInputView = UITextView()
+    let bottomView = UIView()
+
+    let manager = FeedbackManager()
+    var bottomViewHeightConstraint: NSLayoutConstraint!
+    var allowDismiss = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
         layout()
+
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     private func setup() {
+        presentationController?.delegate = self
         view.backgroundColor = .white
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapBackgroundAction)))
+        view.isUserInteractionEnabled = true
+
+        scrollView.keyboardDismissMode = .onDrag
 
         topBarView.backgroundColor = .systemGray5
         topBarView.layer.cornerRadius = 2.0
+
+        sendButton.layer.cornerRadius = 30 / 2
+        sendButton.layer.borderWidth = 1.5
+        sendButton.layer.borderColor = UIColor.systemBlue.cgColor
+        sendButton.isUserInteractionEnabled = true
+        sendButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(sendAction)))
 
         setupLabel(label: titleLable, title: "你想回饋的主題")
         setupInputView(inputView: titleInputView, placeholder: "請輸入想回饋的主題")
@@ -70,44 +97,83 @@ class FeedbackViewController: UIViewController {
             topBarView.heightAnchor.constraint(equalToConstant: 4)
         ])
 
-        view.addSubview(titleLable)
-        titleLable.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(sendButton)
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            titleLable.topAnchor.constraint(equalTo: topBarView.bottomAnchor, constant: 24),
-            titleLable.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24)
+            sendButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            sendButton.heightAnchor.constraint(equalToConstant: 30),
+            sendButton.widthAnchor.constraint(equalToConstant: 30)
         ])
 
-        view.addSubview(titleInputView)
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: topBarView.bottomAnchor, constant: 24),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+
+        scrollView.addSubview(mainContentView)
+        mainContentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mainContentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            mainContentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            mainContentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            mainContentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            mainContentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            mainContentView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+        ])
+
+        mainContentView.addSubview(titleLable)
+        titleLable.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLable.topAnchor.constraint(equalTo: mainContentView.topAnchor),
+            titleLable.leadingAnchor.constraint(equalTo: mainContentView.leadingAnchor, constant: 24)
+        ])
+
+        mainContentView.addSubview(titleInputView)
         titleInputView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             titleInputView.topAnchor.constraint(equalTo: titleLable.bottomAnchor, constant: 6),
             titleInputView.leadingAnchor.constraint(equalTo: titleLable.leadingAnchor, constant: 12),
-            titleInputView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+            titleInputView.trailingAnchor.constraint(equalTo: mainContentView.trailingAnchor, constant: -24)
         ])
 
-        view.addSubview(emailAddressInputView)
+        mainContentView.addSubview(bottomView)
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomView.bottomAnchor.constraint(equalTo: mainContentView.bottomAnchor),
+            bottomView.leadingAnchor.constraint(equalTo: mainContentView.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: mainContentView.trailingAnchor)
+        ])
+        bottomViewHeightConstraint = bottomView.heightAnchor.constraint(equalToConstant: 0)
+        bottomViewHeightConstraint.isActive = true
+
+        mainContentView.addSubview(emailAddressInputView)
         emailAddressInputView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            emailAddressInputView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            emailAddressInputView.bottomAnchor.constraint(equalTo: bottomView.topAnchor),
             emailAddressInputView.leadingAnchor.constraint(equalTo: titleInputView.leadingAnchor),
             emailAddressInputView.trailingAnchor.constraint(equalTo: titleInputView.trailingAnchor)
         ])
 
-        view.addSubview(emailAddressLabel)
+        mainContentView.addSubview(emailAddressLabel)
         emailAddressLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             emailAddressLabel.bottomAnchor.constraint(equalTo: emailAddressInputView.topAnchor, constant: -6),
             emailAddressLabel.leadingAnchor.constraint(equalTo: titleLable.leadingAnchor)
         ])
 
-        view.addSubview(contentLabel)
+        mainContentView.addSubview(contentLabel)
         contentLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             contentLabel.topAnchor.constraint(equalTo: titleInputView.bottomAnchor, constant: 12),
             contentLabel.leadingAnchor.constraint(equalTo: titleLable.leadingAnchor)
         ])
 
-        view.addSubview(contentInputView)
+        mainContentView.addSubview(contentInputView)
         contentInputView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             contentInputView.leadingAnchor.constraint(equalTo: titleInputView.leadingAnchor),
@@ -115,5 +181,57 @@ class FeedbackViewController: UIViewController {
             contentInputView.topAnchor.constraint(equalTo: contentLabel.bottomAnchor, constant: 6),
             contentInputView.bottomAnchor.constraint(equalTo: emailAddressLabel.topAnchor, constant: -12)
         ])
+    }
+
+    @objc private func tapBackgroundAction() {
+        view.endEditing(true)
+    }
+
+    @objc private func sendAction() {
+        Task {
+            await manager.sendFeedback()
+        }
+    }
+}
+
+// MARK: - UIAdaptivePresentationControllerDelegate
+extension FeedbackViewController: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+        return allowDismiss
+    }
+
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        let alert = UIAlertController(title: "要關閉嗎？", message: "資料將會遺失", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "確定關閉", style: .destructive) { [weak self] _ in
+            guard let self = self else { return }
+            self.allowDismiss = true
+            self.dismiss(animated: true)
+        })
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - Keyboard
+extension FeedbackViewController {
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval else { return }
+
+        let keyboardHeight = keyboardFrame.height
+        UIView.animate(withDuration: duration) {
+            self.bottomViewHeightConstraint.constant = keyboardHeight
+            self.scrollView.contentInset.bottom = keyboardHeight
+            self.scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+        }
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        UIView.animate(withDuration: 0.25) {
+            self.bottomViewHeightConstraint.constant = 0
+            self.scrollView.contentInset.bottom = 0
+            self.scrollView.verticalScrollIndicatorInsets.bottom = 0
+        }
     }
 }
